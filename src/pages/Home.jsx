4165bbffc7c396bbb0eb549685b5735daf454cba
Beknur <b4476125@gmail.com>
2025-04-13 15:48:55 +0600

    addad home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>
-      Home
+    <div className='box'>
+      <h1>ghbnj</h1>
+      <h1>tnj</h1>
+      <h1>gyuhij</h1>
+      <h1>yubhijn</h1>
     </div>
   )
 }
